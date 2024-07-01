16e412feb2c2c59f9515914aa3df680179a8ef9b
tarynmitchell <tarynmitchll@gmail.com>
2024-07-01 19:11:36 +0100

    Rendered the synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   console.log(props.meaning);
@@ -9,9 +10,14 @@ export default function Meaning(props) {
         return (
           <div key={index}>
             <p>
-              {definition.definition}
+              <strong>Definition:</strong> {definition.definition}
               <br />
+              <strong>Example:</strong>
               <em>{definition.example}</em>
+              <br />
+              <Synonyms synonyms={definition.synonyms} />
+              <strong>Synonyms:</strong>
+              {definition.synonyms}
             </p>
           </div>
         );
