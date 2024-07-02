170e72c85f11fc95dc17268a37848fd09645d815
tarynmitchell <tarynmitchll@gmail.com>
2024-07-02 17:21:38 +0100

    Styled the application

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -36,7 +36,7 @@ export default function Dictionary(props) {
     return (
       <div className="Dictionary">
         <section>
-          <h1>What word do you want to look up?</h1>
+          <h1>Start by typing any word</h1>
           <form onSubmit={handleSubmit}>
             <input
               type="search"
--- src/Meaning.js
+++ src/Meaning.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Synonyms from "./Synonyms";
+import "./Meaning.css";
 
 export default function Meaning(props) {
   console.log(props.meaning);
@@ -9,16 +10,13 @@ export default function Meaning(props) {
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>
-              <strong>Definition:</strong> {definition.definition}
-              <br />
-              <strong>Example:</strong>
+            <div className="definition">{definition.definition}</div>
+
+            <div className="example">
               <em>{definition.example}</em>
-              <br />
-              <Synonyms synonyms={definition.synonyms} />
-              <strong>Synonyms:</strong>
-              {definition.synonyms}
-            </p>
+            </div>
+
+            <Synonyms synonyms={definition.synonyms} />
           </div>
         );
       })}
